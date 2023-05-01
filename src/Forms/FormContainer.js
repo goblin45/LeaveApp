1c24f53f94b204a29f072d5ab567f2f2eadf8ce8
Rajarshi Dutta <97035849+goblin45@users.noreply.github.com>
2023-05-01 18:36:04 +0530

    Update FormContainer.js

--- src/Forms/FormContainer.js
+++ src/Forms/FormContainer.js
@@ -5,14 +5,20 @@ import Row from 'react-bootstrap/esm/Row';
 
 
 export const FormContainer = ({children}) => {
-  return (
-    <Container width='500px'>
-        <Row className='justify-content-md-center'>
-            <Col sm={10} md={5}>
-              {children}
-            </Col>
-        </Row>
-    </Container>
-  )
+	return (
+		<div className='background'>
+		<div className='overlay'>
+		<Container width='550px' height='650px'>
+			<Row className='justify-content-md-center'>
+				<Col sm={10} md={5}>
+				<div className='form'>
+				{children}
+				</div>
+				</Col>
+			</Row>
+		</Container>
+		</div>
+		</div>
+	)
 }
 
