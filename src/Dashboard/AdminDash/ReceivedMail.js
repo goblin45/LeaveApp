b15d377deec4d8ffb4607ee5506ad287d50848c2
Rajarshi Dutta <97035849+goblin45@users.noreply.github.com>
2023-05-01 18:32:41 +0530

    Update ReceivedMail.js

--- src/Dashboard/AdminDash/ReceivedMail.js
+++ src/Dashboard/AdminDash/ReceivedMail.js
@@ -11,7 +11,7 @@ const ReceivedMail = ({ adminId }) => {
     let _id = adminId
    
     useEffect(() => {
-        axios.post("http://localhost:3500/mails/admins", { _id })
+        axios.post("https://leaveapp-api.onrender.com/mails/admins", { _id })
             .then(response => {
                 const received_mails = response.data
                 setMails(received_mails)
@@ -26,7 +26,7 @@ const ReceivedMail = ({ adminId }) => {
         const status = 'Granted'
         let _id = mail_id
         
-        axios.patch("http://localhost:3500/mails/admins", { _id, status })
+        axios.patch("https://leaveapp-api.onrender.com/mails/admins", { _id, status })
             .then(response => {
                 console.log(_id, status)
                 let data = response.data
@@ -42,7 +42,7 @@ const ReceivedMail = ({ adminId }) => {
         const status = 'Denied'
         let _id = mail_id
 
-        axios.patch("http://localhost:3500/mails/admins", { _id, status })
+        axios.patch("https://leaveapp-api.onrender.com/mails/admins", { _id, status })
             .then(response => { 
                 let data = response.data
                 console.log(data.message)
@@ -57,46 +57,41 @@ const ReceivedMail = ({ adminId }) => {
     return (
         <div className='received'>
             <h4><div className='mail_head'>Here's your pending applications to check</div></h4>
-        
-    
-    {mails?.length ?(
-        mails.map(mail=>(
-        <div className='box'>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th className='t_head2'>Subject</th>
-                        <th className='t_head2'>Days</th>
-                        <th className='t_head2'>Sender</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr key={mail._id}>
-                        <td><p className='t_cont2'>{mail.subject}</p></td>
-                        <td><p className='t_cont2'>{mail.days}</p></td>
-                        <td><p className='t_cont2'>{mail.senderName}</p></td>
-                    </tr>
-                </tbody>
-            </Table>
-            <div key={mail._id} className='row'>
-            <p className='text_area'>{mail.body}</p>
-                    <div className='buttons'>
-                        <Button className='grant' variant='success' onClick={()=>onAcceptClicked(mail._id)}><div className='subhead1'>Grant</div></Button>
-                        <Button className='deny' variant='danger' onClick={()=>onRejectClicked(mail._id)}>Deny</Button>
-                    </div>
-            </div>
 
-            </div>
-            
-        ))
+        {mails?.length ?(
+            mails.map(mail=>(
+            <div className='box'>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th className='t_head2'>Subject</th>
+                            <th className='t_head2'>Days</th>
+                            <th className='t_head2'>Sender</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr key={mail._id}>
+                            <td><p className='t_cont2'>{mail.subject}</p></td>
+                            <td><p className='t_cont2'>{mail.days}</p></td>
+                            <td><p className='t_cont2'>{mail.senderName}</p></td>
+                        </tr>
+                    </tbody>
+                </Table>
+                <div key={mail._id} className='row'>
+                <p className='text_area'>{mail.body}</p>
+                        <div className='buttons'>
+                            <Button className='grant' variant='success' onClick={()=>onAcceptClicked(mail._id)}><div className='subhead1'>Grant</div></Button>
+                            <Button className='deny' variant='danger' onClick={()=>onRejectClicked(mail._id)}>Deny</Button>
+                        </div>
+                </div>
 
-    ):<p><div className='no_mail'> No applications Found</div> </p>}
-    </div>
-   
+                </div>
+                
+            ))
 
-    )
-        
-    
+        ):<p><div className='no_mail'> No applications Found</div> </p>}
+        </div>
+    )  
 }
 
 export default ReceivedMail
