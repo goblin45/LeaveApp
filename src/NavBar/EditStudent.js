f1b893944d04918996353297c19dd562a234d82f
Rajarshi Dutta <97035849+goblin45@users.noreply.github.com>
2023-05-06 20:58:50 +0530

    Update EditStudent.js

--- src/NavBar/EditStudent.js
+++ src/NavBar/EditStudent.js
@@ -20,7 +20,7 @@ const EditStudent = () => {
     const { _id } = location.state || {}
 
     useEffect(() => {
-        axios.post('https://leaveapp-api.onrender.com/students/find', { _id })
+        axios.post('http://localhost:3500/students/find', { _id })
             .then(response => {
                 const student = response.data
                 console.log(student)
@@ -37,7 +37,7 @@ const EditStudent = () => {
         e.preventDefault()
         console.log(id, name, inst_name, password)
 
-        axios.patch('https://leaveapp-api.onrender.com/students', { _id, id, name, password, inst_name })
+        axios.patch('http://localhost:3500/students', { _id, id, name, password, inst_name })
             .then(response => {
                 const data = response.data
                 // console.log(data)
@@ -52,69 +52,71 @@ const EditStudent = () => {
 
     return (
         <div>
-            <div className='bagc'>
-            <NavBarDashBoard
-                student_id={_id}
-                admin_id = {''}
-            />
-            <FormContainer>
-                <h2><div className='head1'>Edit Profile</div></h2>
-                <Form.Group controlId="id">
-                <div className='subhead1'>
-                    <Form.Label><h4>Student Id</h4></Form.Label>
-                    <Form.Control
-                        type='text'
-                        name="id"
-                        value={id}
-                        onChange={(e) => setId(e.target.value)}
-                    />
-                    </div>
-                </Form.Group>
+        <div className='bagc'>
+        <NavBarDashBoard
+            student_id={_id}
+            admin_id = {''}
+        />
+        <FormContainer>
+            <h2><div className='form_head'>Edit Profile</div></h2>
+            <hr className='md-3'/>
+            <Form.Group controlId="id">
+            <div className='subhead1'>
+                <Form.Label><h4>Student Id</h4></Form.Label>
+                <Form.Control
+                    type='text'
+                    name="id"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                />
+                </div>
+            </Form.Group>
 
-                <Form.Group controlId="name">
-                <div className='subhead1'>
-                    <Form.Label><h4>Name</h4></Form.Label>
-                    <Form.Control
-                        type='text'
-                        name="name"
-                        value={name}
-                        onChange={(e)=>setName(e.target.value)}
-                    />
-                    </div>
-                </Form.Group>
+            <Form.Group controlId="name">
+            <div className='subhead1'>
+                <Form.Label><h4>Name</h4></Form.Label>
+                <Form.Control
+                    type='text'
+                    name="name"
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
+                />
+                </div>
+            </Form.Group>
 
-                <Form.Group controlId="password">
-                <div className='subhead1'>
-                    <Form.Label><h4>Password</h4></Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder="password"
-                        name="password"
-                        value={password}
-                        onChange={(e)=>setPassword(e.target.value)}
-                    />
-                    </div>
-                </Form.Group>
+            <Form.Group controlId="password">
+            <div className='subhead1'>
+                <Form.Label><h4>Password</h4></Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder="password"
+                    name="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                />
+                </div>
+            </Form.Group>
 
-                <Form.Group controlId="inst_name">
-                <div className='subhead1'>
-                    <Form.Label><h4>Institution Name</h4></Form.Label>
-                    <Form.Control
-                        type='text'
-                        name="inst_name"
-                        value={inst_name}
-                        onChange={(e)=>setInst_Name(e.target.value)}
-                    />
-                    </div>
-                </Form.Group>
+            <Form.Group controlId="inst_name">
+            <div className='subhead1'>
+                <Form.Label><h4>Institution Name</h4></Form.Label>
+                <Form.Control
+                    type='text'
+                    name="inst_name"
+                    value={inst_name}
+                    onChange={(e)=>setInst_Name(e.target.value)}
+                />
+                </div>
+            </Form.Group>
 
-                {err?.length ? (<p>{err}</p>) : <></>}	
+            {err?.length ? (<p>{err}</p>) : <></>}
+            <hr className='md-3'/>	
 
-                <Button onClick={handleUpdate}><div className='subhead1'>Update Details</div></Button>
+            <Button onClick={handleUpdate}><div className='update1'>Update Details</div></Button>
 
-            </FormContainer>
-            </div>
+        </FormContainer>
         </div>
+    </div>
     )
 }
 
