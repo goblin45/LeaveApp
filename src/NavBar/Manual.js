f38898fa7d3ce684768938b1425b9d163c4825f8
Rajarshi Dutta <97035849+goblin45@users.noreply.github.com>
2023-05-10 12:55:54 +0530

    Update Manual.js

--- src/NavBar/Manual.js
+++ src/NavBar/Manual.js
@@ -1,5 +1,5 @@
 import NavBar from "./NavBar"
-import image3 from "../images/compose_mail.jpg"
+import image3 from "../images/compose_mail.png"
 import image4 from "../images/admin sign up1.png"
 import image5 from "../images/Admin sign up2.png"
 import image6 from "../images/student signup.png"
@@ -7,6 +7,7 @@ import image7 from "../images/student sign in.png"
 import image8 from "../images/admin log in.png"
 import image9 from "../images/admin dashboard.png"
 import image10 from "../images/student dashboard.png"
+import image11 from "../images/student dashboard after.png";
 
 
 const Manual = () => {
@@ -37,10 +38,14 @@ const Manual = () => {
                         <img className='image_manual' src={image8} alt="image8"/>
                         <p><br></br><div className='manualcontentext'>6) Mention your subject, your respective department head, number of days you want to take a leave for and state your cause in the following boxes.</div></p>
                         <img className='image_manual' src={image3} alt="image3"/>
-                        <p><br></br><div className='manualcontentext'>7) You can find all your unseen and unchecked applications. Should you choose to approve the application, click on <i><b>“Grant”, “Deny”</b></i> otherwise.</div></p>
+                        <p><br></br><div className='manualcontentext'>7) The admin dashboard is now present with the application just being composed. The application can be granted or denied by clicking on <i><b>“Grant” or “Deny”</b></i> </div></p>
                         <img className='image_manual' src={image9} alt="image9"/>
-                        <p><br></br><div className='manualcontentext'>8) This is a library of all your pending mails under the <i><b>“Your Pending Applications”</b></i> and previously sent mails under the<i><b> “Your Past Applications”</b></i>, stating the <i><b> “Subject”</b></i>, their <i><b>“Status”</b></i>, the professor you <i><b>“Sent to”</b></i>. Should you wish to edit your context, click on <i><b>“Edit”</b></i>, and if you want to delete your mail from the Dashboard, click on <i><b>“Delete”</b></i>. Click on <i><b>“Compose Now”</b></i> if you wish to create a new application. </div></p>
+                        <p><br></br><div className='manualcontentext'>8) In the student dashboard, the application just sent is present in  <i><b>“Your Pending Applications”</b></i>table as it is not being responded by the concerned admin yet. The columns are clearly representing the components of your application i.e. <i><b> “Subject”</b></i>, their <i><b>“Status”</b></i>, the professor you <i><b>“Sent to”</b></i>. Should you wish to edit your context, click on <i><b>“Edit”</b></i>, and if you want to delete your mail from the Dashboard, click on <i><b>“Delete”</b></i>. Click on <i><b>“Compose New”</b></i> if you wish to create another new application. </div></p>
                         <img className='image_manual' src={image10} alt="image10"/>
+                        <p><br></br><div className='manualcontentext'>9) Now after the admin responded to the application, it will shift from  <i><b>“Your Pending Applications”</b></i> to <i><b> “Your Past Applications”</b></i>, where the columns are <i><b> “Subject”</b></i>, their <i><b>“Status”</b></i>,(Granted or denied) the concerned admin you <i><b>“Concerned admin”</b></i>. Should you wish to delete your application, click on <i><b>“Delete”</b></i></div></p>
+                        <img className='image_manual' src={image11} alt="image11"/>
+                        
+
                     
                       </ol>  
                 </div>
